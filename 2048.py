94a6302ecd3985995ca1fb7ef2743671c505279b
unknown <santosbasty4@gmail.com>
2023-02-17 15:32:27 +0800

    Second Commit - Fixed Functionality for Vertical

--- 2048.py
+++ 2048.py
@@ -1,7 +1,8 @@
 import random
 
-# Initialize the board
+# Initialize the board and score
 board = [[0 for i in range(4)] for j in range(4)]
+score = 0
 
 # Add two random tiles to the board
 def add_random_tile():
@@ -12,36 +13,54 @@ def add_random_tile():
 
 # Move tiles in the specified direction
 def move(direction):
+    global score
     moved = False
     for i in range(4):
-        row = board[i]
         if direction == 'left':
-            row = [cell for cell in row if cell != 0] + [0] * row.count(0)
+            row = board[i]
+            new_row = [cell for cell in row if cell != 0] + [0] * row.count(0)
+            if new_row != row:
+                moved = True
+            board[i] = new_row
         elif direction == 'right':
-            row = [0] * row.count(0) + [cell for cell in row if cell != 0][::-1]
+            row = board[i]
+            new_row = [0] * row.count(0) + [cell for cell in row if cell != 0]
+            if new_row != row:
+                moved = True
+            board[i] = new_row
         elif direction == 'up':
             col = [board[j][i] for j in range(4)]
-            col = [cell for cell in col if cell != 0] + [0] * col.count(0)
+            new_col = [cell for cell in col if cell != 0] + [0] * col.count(0)
+            if new_col != col:
+                moved = True
             for j in range(4):
-                board[j][i] = col[j]
+                board[j][i] = new_col[j]
         elif direction == 'down':
-            col = [board[j][i] for j in range(4)][::-1]
-            col = [cell for cell in col if cell != 0] + [0] * col.count(0)
+            col = [board[j][i] for j in range(4)]
+            new_col = [0] * col.count(0) + [cell for cell in col if cell != 0]
+            if new_col != col:
+                moved = True
             for j in range(4):
-                board[j][i] = col[3-j]
-        if row != board[i]:
-            moved = True
-            board[i] = row
+                board[j][i] = new_col[j]
+    if moved:
+        add_random_tile()
     return moved
 
 # Merge adjacent tiles with the same value
 def merge():
+    global score
     merged = False
     for i in range(4):
         for j in range(3):
             if board[i][j] != 0 and board[i][j] == board[i][j+1]:
                 board[i][j] *= 2
                 board[i][j+1] = 0
+                score += board[i][j]
+                merged = True
+            if board[j][i] != 0 and board[j][i] == board[j+1][i]:
+                board[j][i] *= 2
+                board[j+1][i] = 0
+                score += board[j][i]
                 merged = True
     return merged
 
@@ -61,6 +80,14 @@ def is_game_over():
                 return False
     return True
 
+# Check if the player has won
+def has_won():
+    for i in range(4):
+        for j in range(4):
+            if board[i][j] == 2048:
+                return True
+    return False
+
 # Play the game
 add_random_tile()
 while not is_game_over():
@@ -70,5 +97,9 @@ while not is_game_over():
     merged = merge()
     if moved or merged:
         add_random_tile()
+    if has_won():
+        print_board()
+        print('Congratulations! You won!')
+        break
 print_board()
-print('Game over!')
+print('Game over! Your score is:', score)
